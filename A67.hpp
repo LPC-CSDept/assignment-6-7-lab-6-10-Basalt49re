4bf4971c7a323429114ca23b77015635fcc3f300
Albert <ayang04@zohomail.com>
2022-10-10 13:43:29 -0700

    try again

--- A67.hpp
+++ A67.hpp
@@ -1,12 +1,7 @@
-//
-
 #include <iomanip>
 #include <iostream>
 using namespace std;
 
-// complete the Overloading function swap( )
-// swap ( ) function has the difference number of parameters from 2 to 4
-// For example, swap(10, 20); swap(10, 20, 30); swap(10, 20, 30, 40)
 void swap(int &num1,int &num2)
 {
   int temp = num1;
@@ -25,30 +20,30 @@ void swap(int &num1,int &num2, int &num3)
    } else {
       max = num3;
    }
-    
-   int smallest = num1;
-   int min;
-   if (num2< min)
-    min = num2;
-   if (num3 <min)
-    min = num3;
 
-    if (num1 != min && num1 !=max){
-        remainder =num1;
-    }
-    if (num2 != min && num2 !=max){
-        remainder =num2;}
+  int smallest = num1;
+  int min;
+  if (num2 < min)
+  min=num2;
+  if(num3 < min)
+  min=num3;
 
-    if (num3 != min && num3 !=max){
-        remainder =num3;}
+  if(num1 != min && num1 != max ){
+    remainder = num1;}
+  if(num2 != min && num2 != max ){
+    remainder = num2;}
+  if(num3 != min && num3 != max ){
+    remainder = num3;}
     num1 = min;
     num2 = remainder;
     num3 = max;
-    
-    
 }
-void swap (int &num1, int &num2, int &num3, int &num4){
-    swap(num1,num2);
-    swap(num2,num4);
-    swap(num2,num3);
+
+
+void swap(int &num1,int &num2, int &num3, int &num4)
+{
+  swap(num1,num2);
+  swap(num2,num4);
+  swap(num2,num3);
+
 }
